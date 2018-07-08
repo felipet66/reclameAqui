ffd5a3ece1f1b7b695a5d69f3643f2aac06c2236
Unknown <fliptm95@gmail.com>
2018-07-08 16:00:50 -0300

    Return complaint by Id and treatments

--- src/main/java/com/felipeteles/reclameaqui/resources/ComplaintResource.java
+++ src/main/java/com/felipeteles/reclameaqui/resources/ComplaintResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,7 @@ import com.felipeteles.reclameaqui.dto.ComplaintDTO;
 import com.felipeteles.reclameaqui.services.ComplaintService;
 
 @RestController
-@RequestMapping(value="/reclamacoes")
+@RequestMapping(value="/api/complains")
 public class ComplaintResource {
 	
 	@Autowired
@@ -25,5 +26,12 @@ public class ComplaintResource {
 		List<Complaint> list = service.findAll();
 		List<ComplaintDTO> listDto = list.stream().map(x -> new ComplaintDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<ComplaintDTO> findById(@PathVariable String id) {	
+		Complaint complaintObj = service.findById(id);
+		
+		return ResponseEntity.ok().body(new ComplaintDTO(complaintObj));
 	}
 }
--- src/main/java/com/felipeteles/reclameaqui/services/ComplaintService.java
+++ src/main/java/com/felipeteles/reclameaqui/services/ComplaintService.java
@@ -1,12 +1,14 @@
 package com.felipeteles.reclameaqui.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipeteles.reclameaqui.domain.Complaint;
 import com.felipeteles.reclameaqui.repository.ComplaintRepository;
+import com.felipeteles.reclameaqui.services.exception.ObjectNotFoundException;
 
 @Service
 public class ComplaintService {
@@ -16,5 +18,10 @@ public class ComplaintService {
 	
 	public List<Complaint> findAll(){
 		return repo.findAll();
+	}
+	
+	public Complaint findById(String id) {
+		Optional<Complaint> complaintObj = repo.findById(id);
+		return complaintObj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
