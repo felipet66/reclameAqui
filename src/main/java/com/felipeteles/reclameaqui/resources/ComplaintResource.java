0f4ae9002f614fae5d287482bd65ade49def1165
Unknown <fliptm95@gmail.com>
2018-07-08 16:11:32 -0300

    Inserting of complaint with POST

--- src/main/java/com/felipeteles/reclameaqui/resources/ComplaintResource.java
+++ src/main/java/com/felipeteles/reclameaqui/resources/ComplaintResource.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.felipeteles.reclameaqui.domain.Complaint;
 import com.felipeteles.reclameaqui.dto.ComplaintDTO;
@@ -31,7 +33,14 @@ public class ComplaintResource {
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<ComplaintDTO> findById(@PathVariable String id) {	
 		Complaint complaintObj = service.findById(id);
-		
 		return ResponseEntity.ok().body(new ComplaintDTO(complaintObj));
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody ComplaintDTO complaintDtoObj) {	
+		Complaint complaintObj = service.fromDTO(complaintDtoObj);
+		complaintObj = service.insert(complaintObj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(complaintObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/felipeteles/reclameaqui/services/ComplaintService.java
+++ src/main/java/com/felipeteles/reclameaqui/services/ComplaintService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipeteles.reclameaqui.domain.Complaint;
+import com.felipeteles.reclameaqui.dto.ComplaintDTO;
 import com.felipeteles.reclameaqui.repository.ComplaintRepository;
 import com.felipeteles.reclameaqui.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,14 @@ public class ComplaintService {
 	public Complaint findById(String id) {
 		Optional<Complaint> complaintObj = repo.findById(id);
 		return complaintObj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public Complaint insert(Complaint complaintObj) {
+		return repo.insert(complaintObj);
+	}
+	
+	public Complaint fromDTO(ComplaintDTO complaintDtoObj) {
+		return new Complaint(complaintDtoObj.getId(), complaintDtoObj.getTitle(), 
+				complaintDtoObj.getDescription(), complaintDtoObj.getLocale(), complaintDtoObj.getCompany());
 	}
 }
