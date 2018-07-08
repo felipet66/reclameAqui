eccb128c6f55fe662c7b61057b00092252c47c15
Unknown <fliptm95@gmail.com>
2018-07-08 17:09:37 -0300

    Refactoring with lombok

--- src/main/java/com/felipeteles/reclameaqui/domain/Complaint.java
+++ src/main/java/com/felipeteles/reclameaqui/domain/Complaint.java
@@ -2,96 +2,38 @@ package com.felipeteles.reclameaqui.domain;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotNull;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 @Document
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class Complaint implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	private String id;
+	
+	@NotNull(message = "Field \"title\" can not be emptys.")
 	private String title;
+	
+	@NotNull(message = "Field \"description\" can not be emptys.")
 	private String description;
+	
+	@NotNull(message = "Field \"locale\" can not be emptys.")
 	private String locale;
+	
+	@NotNull(message = "Field \"company\" can not be emptys.")
 	private String company;
-	
-	public Complaint() {
-		
-	}
-
-	public Complaint(String id, String title, String description, String locale, String company) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.description = description;
-		this.locale = locale;
-		this.company = company;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getLocale() {
-		return locale;
-	}
-
-	public void setLocale(String locale) {
-		this.locale = locale;
-	}
-
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Complaint other = (Complaint) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
 }
--- src/main/java/com/felipeteles/reclameaqui/dto/ComplaintDTO.java
+++ src/main/java/com/felipeteles/reclameaqui/dto/ComplaintDTO.java
@@ -2,20 +2,36 @@ package com.felipeteles.reclameaqui.dto;
 
 import java.io.Serializable;
 
-import com.felipeteles.reclameaqui.domain.Complaint;
+import javax.validation.constraints.NotNull;
 
+import com.felipeteles.reclameaqui.domain.Complaint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class ComplaintDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private String id;
-	private String title;
-	private String description;
-	private String locale;
-	private String company;
 	
-	public ComplaintDTO() {
-		
-	}
+	@NotNull(message = "Field \"title\" can not be emptys.")
+	private String title;
+	
+	@NotNull(message = "Field \"description\" can not be emptys.")
+	private String description;
+	
+	@NotNull(message = "Field \"locale\" can not be emptys.")
+	private String locale;
+	
+	@NotNull(message = "Field \"company\" can not be emptys.")
+	private String company;
 	
 	public ComplaintDTO(Complaint complaintObj) {
 		id 			= complaintObj.getId();
@@ -23,46 +39,5 @@ public class ComplaintDTO implements Serializable{
 		description = complaintObj.getDescription();
 		locale		= complaintObj.getLocale();
 		company 	= complaintObj.getCompany();
-		
-	}
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getLocale() {
-		return locale;
-	}
-
-	public void setLocale(String locale) {
-		this.locale = locale;
-	}
-
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
 	}
 }
