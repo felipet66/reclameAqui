ef1cda687de1d039d78f29791f8b2af921650d55
Unknown <fliptm95@gmail.com>
2018-07-08 12:22:14 -0300

    Project created -reclameaqui

---
+++ src/main/java/com/felipeteles/reclameaqui/ReclameAquiApplication.java
@@ -0,0 +1,12 @@
+package com.felipeteles.reclameaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReclameAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReclameAquiApplication.class, args);
+	}
+}
